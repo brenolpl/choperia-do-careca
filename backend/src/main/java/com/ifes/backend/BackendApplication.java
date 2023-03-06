342b065cd759c7ff891a138e27610cbcef3a2d89
Breno Luiz <brenoluizleal@gmail.com>
2023-03-06 20:33:40 -0300

    impressão codigo barras

--- backend/src/main/java/com/ifes/backend/BackendApplication.java
+++ backend/src/main/java/com/ifes/backend/BackendApplication.java
@@ -1,8 +1,12 @@
 package com.ifes.backend;
 
 
+import com.aspose.barcode.EncodeTypes;
+import com.aspose.barcode.generation.BarcodeGenerator;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -11,4 +15,14 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Bean
+	CommandLineRunner run(){
+		return args -> {
+            BarcodeGenerator generator = new BarcodeGenerator(EncodeTypes.EAN_13, "test code");
+// set resolution
+            generator.getParameters().setResolution(203);
+// generate barcode
+            generator.save("generate-barcode.png");
+		};
+	}
 }

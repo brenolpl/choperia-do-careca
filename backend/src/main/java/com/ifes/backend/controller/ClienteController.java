2729f3f1a500159b879920c15448b0a98de39e0d
João Vitor <joao.natali@vixteam.com.br>
2023-03-21 13:37:39 -0300

    Criando servidor do microterminal

--- backend/src/main/java/com/ifes/backend/controller/ClienteController.java
+++ backend/src/main/java/com/ifes/backend/controller/ClienteController.java
@@ -2,6 +2,8 @@ package com.ifes.backend.controller;
 
 import com.ifes.backend.domain.Cliente;
 import com.ifes.backend.persistence.IClienteRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,11 @@ public class ClienteController extends BaseController<Cliente, IClienteRepositor
 
     public ClienteController(IClienteRepository repository) {
         super(Cliente.class, repository);
+    }
+
+    @GetMapping("{codigoRFID}/totalConta")
+    public Double getTotalContaCliente(@PathVariable String codigoRFID){
+        if(codigoRFID.equals("33")) throw new RuntimeException("Cliente não encontrado!");
+        return 15.5;
     }
 }
